e11478f44f7b232e216f2e45d20c655638508ac9
saloev <saloev.saadi@yandex.ru>
2020-10-04 19:56:28 +0300

    fix: add icons to gitignore

--- src/icons/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-require('./burger')
-require('./chevron-bottom')
-require('./list-notification')
-require('./list')
-require('./location')
-require('./search')
-require('./spin')
